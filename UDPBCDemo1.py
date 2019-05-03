a1163c843430d5eb30db25ec5607d05cb51bddba
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-03 10:25:03 +0200

    Create UDPBCDemo1.py

---
+++ UDPBCDemo1.py
@@ -0,0 +1,17 @@
+BROADCAST_TO_PORT = 15000
+import time
+from socket import *
+from datetime import datetime
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+s = socket(AF_INET, SOCK_DGRAM)
+#s.bind(('', 14593))     # (ip, port)
+# no explicit bind: will bind to default IP + random port
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+while True:
+	data = "Current time: " + str(datetime.now()) + "\n" + "Temperatur: " + sense.get_temperature_from_humidity + " Celsius"
+	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+	print(data)
+	time.sleep(5)
