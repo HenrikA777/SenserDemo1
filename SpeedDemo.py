55239f8e2563d00966e64d1964a090faf4e1b7ad
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-15 16:54:24 +0200

    Update SpeedDemo.py

--- SpeedDemo.py
+++ SpeedDemo.py
@@ -1,9 +1,11 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
 speed = 0
 p = 0
 blue = (0, 0, 255)
+sense.clear()
 
 
 while True:
@@ -15,6 +17,7 @@ while True:
 	speed = speed * 0.99
 	
 	p += 1
+	time.sleep(0.01)
   
 	if (x > 0.1 or x < -0.1):
 	  speed += x
@@ -22,10 +25,12 @@ while True:
 	if (p == 10):
 	  print(speed)
 	  p = 0
-	  sense.clear
+	  sense.clear()
 	  if (speed > 2 and speed < 4):
 	    sense.set_pixel(5, 5, blue)
 	  if (speed > 4):
 	    sense.set_pixel(6, 5, blue)
 	  if (speed < -2 and speed > -4):
 	    sense.set_pixel(4, 5, blue)
+	  if (speed < -4):
+	    sense.set_pixel(3, 5, blue)
